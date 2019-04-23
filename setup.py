93a9fef4d91b4dcdc13379bc7b3c611685d588b7
ryanlovett <rylo@berkeley.edu>
2019-04-23 10:13:18 -0700

    Bump to 0.2.

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import setuptools
 
 setuptools.setup(
 	name='sis',
-	version='0.1',
+	version='0.2',
 	description='Query the UC Berkeley SIS.',
 	url='https://github.com/ryanlovett/sis',
 	author='Ryan Lovett',
 	author_email='rylo@berkeley.edu',
 	packages=setuptools.find_packages(),
 	install_requires=[
-	  'requests'
+	  'aiohttp'
 	],
     entry_points={
         'console_scripts': [
-            'sis= sis.__main__:main',
+            'sis= sis.__main__:run',
         ],
     },
 
