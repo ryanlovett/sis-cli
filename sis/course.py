93e766e104b810670ac0eb6000e5dab1a328fe42
ryanlovett <rylo@berkeley.edu>
2025-02-18 12:59:22 -0800

    Add function to get current courses.
    The API returns courses with old fromData and toDate. Provide a convenience function that only returns courses between those dates.
    
    I'm not sure under what other circumstances the API will return more than one course. If the time range is the only factor, then it may be useful to return a dictionary rather than a list of a single dictionary.

--- sis/course.py
+++ sis/course.py
@@ -1,4 +1,5 @@
 # vim:set et sw=4 ts=4:
+from datetiem import datetime
 import json
 import logging
 import sys
@@ -22,12 +23,33 @@ async def get_courses(app_id, app_key, **kwargs):
         return []
     uri = classes_uri
     params = kwargs
-    if 'sort-by' not in params:
-        params['sort-by'] = 'catalog-number'
-    params['page-number'] = 1
+    if "sort-by" not in params:
+        params["sort-by"] = "catalog-number"
+    params["page-number"] = 1
 
     courses = await sis.get_items(uri, params, headers, "courses")
     return courses
+
+
+async def get_current_courses(app_id, app_key, **kwargs):
+    # Call the original get_courses function
+    courses = await get_courses(app_id, app_key, **kwargs)
+
+    # Get the current date
+    current_date = datetime.now().date()
+
+    # Filter the courses based on the current date
+    filtered_courses = [
+        course
+        for course in courses
+        if "fromDate" in course
+        and "toDate" in course
+        and datetime.strptime(course["fromDate"], "%Y-%m-%d").date()
+        <= current_date
+        <= datetime.strptime(course["toDate"], "%Y-%m-%d").date()
+    ]
+
+    return filtered_courses
 
 
 def print_courses(courses):
