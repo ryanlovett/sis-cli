b33fdf46f51e508145a2e7b66f32ad9f4d9794f9
Ryan Lovett <rylo@berkeley.edu>
2026-02-13 12:53:17 -0800

    Run black.

--- sis/student.py
+++ sis/student.py
@@ -13,61 +13,64 @@ logger = logging.getLogger(__name__)
 # Various SIS endpoints
 students_url = "https://gateway.api.berkeley.edu/sis/v2/students"
 
-async def get_student(app_id, app_key, identifier, id_type, item_key):
-    '''Given a term and class section ID, return section data.'''
-    uri = f'{students_url}/{identifier}'
 
-    headers = {
-        "Accept": "application/json",
-        "app_id": app_id,
-        "app_key": app_key
-    }
+async def get_student(app_id, app_key, identifier, id_type, item_key):
+    """Given a term and class section ID, return section data."""
+    uri = f"{students_url}/{identifier}"
+
+    headers = {"Accept": "application/json", "app_id": app_id, "app_key": app_key}
     params = {
         "id-type": id_type,
-        "inc-acad": "true", "inc-attr": "true",
-        "inc-regs": "false", "inc-cntc": "false", "inc-regs": "false",
-        "inc-dmgr": "false", "inc-work": "false", "inc-dob": "false",
+        "inc-acad": "true",
+        "inc-attr": "true",
+        "inc-regs": "false",
+        "inc-cntc": "false",
+        "inc-regs": "false",
+        "inc-dmgr": "false",
+        "inc-work": "false",
+        "inc-dob": "false",
         "inc-gndr": "false",
-        "affiliation-status": "ACT", "inc-completed-programs": "true",
+        "affiliation-status": "ACT",
+        "inc-completed-programs": "true",
     }
     logger.debug(f"status for {identifier}")
     items = await sis.get_items(uri, params, headers, item_key)
     logger.debug(f"items: {items}")
     return items
 
+
 async def get_academic_statuses(app_id, app_key, identifier, id_type):
-    '''Given a student identifier, return student academic statuses.'''
-    items = await get_student(app_id, app_key, identifier, id_type, 'academicStatuses')
+    """Given a student identifier, return student academic statuses."""
+    items = await get_student(app_id, app_key, identifier, id_type, "academicStatuses")
     return items
 
-async def get_name(app_id, app_key, identifier, id_type, code='Preferred'):
-    '''Given a student identifier, return student names.'''
-    items = await get_student(app_id, app_key, identifier, id_type, 'names')
+
+async def get_name(app_id, app_key, identifier, id_type, code="Preferred"):
+    """Given a student identifier, return student names."""
+    items = await get_student(app_id, app_key, identifier, id_type, "names")
     names = []
     for item in items:
-        if jmespath.search('type.description', item) != code:
+        if jmespath.search("type.description", item) != code:
             continue
-        sn = jmespath.search('familyName', item)
-        givenName = jmespath.search('givenName', item)
+        sn = jmespath.search("familyName", item)
+        givenName = jmespath.search("givenName", item)
         return f"{sn},{givenName}"
+
 
 def get_academic_plans(status):
     logger.debug(f"status: {status}")
-    plans = status.get('studentPlans', [])
+    plans = status.get("studentPlans", [])
     logger.debug(f"plans: {plans}")
 
-    return list(map(lambda x: x['academicPlan']['plan'], plans))
+    return list(map(lambda x: x["academicPlan"]["plan"], plans))
 
-async def get_emails(app_id, app_key, identifier, id_type='campus-id'):
-    '''Given a identifier, return the student's email address.'''
-    uri = f'{students_url}/{identifier}'
-    item_key = 'emails'
 
-    headers = {
-        "Accept": "application/json",
-        "app_id": app_id,
-        "app_key": app_key
-    }
+async def get_emails(app_id, app_key, identifier, id_type="campus-id"):
+    """Given a identifier, return the student's email address."""
+    uri = f"{students_url}/{identifier}"
+    item_key = "emails"
+
+    headers = {"Accept": "application/json", "app_id": app_id, "app_key": app_key}
     params = {
         "id-type": id_type,
         "inc-cntc": "true",
--- sis/terms.py
+++ sis/terms.py
@@ -11,57 +11,51 @@ logger = logging.getLogger(__name__)
 # SIS endpoint
 terms_uri = "https://gateway.api.berkeley.edu/sis/v2/terms"
 
-async def get_term_name(app_id, app_key, term_id):
-    '''Given a term id, return the term's friendly name.'''
-    uri = f'{terms_uri}/{term_id}'
-    headers = {
-        "Accept": "application/json",
-        "app_id": app_id, "app_key": app_key
-    }
-    terms = await sis.get_items(uri, params, headers, 'terms')
-    if len(terms) == 0: # if we are between terms
-        return None
-    return terms[0]['name']
 
-async def get_term_id(app_id, app_key, position='Current'):
-    '''Given a temporal position of Current, Previous, or Next, return
-       the corresponding term's ID.'''
-    uri = terms_uri
-    headers = {
-        "Accept": "application/json",
-        "app_id": app_id, "app_key": app_key
-    }
-    params = { "temporal-position": position }
-    terms = await sis.get_items(uri, params, headers, 'terms')
-    logger.debug(f"terms: {terms}")
-    if len(terms) == 0: # if we are between terms
+async def get_term_name(app_id, app_key, term_id):
+    """Given a term id, return the term's friendly name."""
+    uri = f"{terms_uri}/{term_id}"
+    headers = {"Accept": "application/json", "app_id": app_id, "app_key": app_key}
+    terms = await sis.get_items(uri, params, headers, "terms")
+    if len(terms) == 0:  # if we are between terms
         return None
-    return terms[0]['id']
+    return terms[0]["name"]
+
+
+async def get_term_id(app_id, app_key, position="Current"):
+    """Given a temporal position of Current, Previous, or Next, return
+    the corresponding term's ID."""
+    uri = terms_uri
+    headers = {"Accept": "application/json", "app_id": app_id, "app_key": app_key}
+    params = {"temporal-position": position}
+    terms = await sis.get_items(uri, params, headers, "terms")
+    logger.debug(f"terms: {terms}")
+    if len(terms) == 0:  # if we are between terms
+        return None
+    return terms[0]["id"]
+
 
 async def get_term_id_from_year_sem(app_id, app_key, year, semester):
-    '''Given a year and Berkeley semester, return the corresponding
-       term's ID.'''
-    headers = {
-        "Accept": "application/json",
-        "app_id": app_id, "app_key": app_key
-    }
-    if semester == 'spring':
-        mm_dd = '02-01'
-    elif semester == 'summer':
-        mm_dd = '07-01'
-    elif semester == 'fall':
-        mm_dd = '10-01'
+    """Given a year and Berkeley semester, return the corresponding
+    term's ID."""
+    headers = {"Accept": "application/json", "app_id": app_id, "app_key": app_key}
+    if semester == "spring":
+        mm_dd = "02-01"
+    elif semester == "summer":
+        mm_dd = "07-01"
+    elif semester == "fall":
+        mm_dd = "10-01"
     else:
         raise Exception(f"No such semester: {semester}")
-    params = { "as-of-date": f"{year}-{mm_dd}" }
+    params = {"as-of-date": f"{year}-{mm_dd}"}
     uri = terms_uri
-    terms = await sis.get_items(uri, params, headers, 'terms')
-    return terms[0]['id']
+    terms = await sis.get_items(uri, params, headers, "terms")
+    return terms[0]["id"]
+
 
 async def normalize_term_id(app_id, app_key, term_id):
-    '''Convert temporal position (current, next, previous) to a numeric term id,
-       or passthrough a numeric term id.'''
+    """Convert temporal position (current, next, previous) to a numeric term id,
+    or passthrough a numeric term id."""
     if term_id.isalpha():
         term_id = await get_term_id(app_id, app_key, term_id)
     return term_id
-
